03adc38bf1b693ee1b77ecb093e2e316e35d89db
Maryam Zraydi <maryam@example.com>
2025-12-13 17:54:30 +0100

    Initialize DB connection for Zoo Encyclopedia project

--- config/db.php
+++ config/db.php
@@ -12,3 +12,5 @@ $conn = mysqli_connect($host, $user, $password, $database);
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
+
+mysqli_set_charset($conn, "utf8");
